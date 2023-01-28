b43666ec6d4c4cb56cf6d514861e78c9649dd460
jiangmuran <wd.jiang@foxmail.com>
2023-01-28 21:21:20 +0800

    Update robotmodel.py

--- robotmodel.py
+++ robotmodel.py
@@ -232,7 +232,7 @@ recv():
 
   def sendw(self,nick,text):
     try:
-      self.sendcmd('{"cmd":"whisper","nick":'+nick+',"text":"'+text+'""}')
+      self.sendcmd('{"cmd":"whisper","nick":"'+nick+'","text":"'+text+'""}')
     except:
       print("error,reconnect in "+str(self.reconn_time)+"s")
       time.sleep(self.reconn_time)
